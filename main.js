da3eec6f5a4a5ccb5f995d5f3c05f0fbc5946658
sridharhitman <83956232+sridharhitman@users.noreply.github.com>
2022-03-16 15:58:04 +0530

    complete

--- main.js
+++ main.js
@@ -15,7 +15,22 @@ function take_snapshot(){
 }
 
 console.log('ml5 Version', ml5.version);
-classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/v_sl95BzE/model.json', modelLoaded);
+classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/noWYzbBCi/model.json', modelLoaded);
 function modelLoaded(){
     console.log("Model is Loaded!!!!!!!!!!!!!!!!")
 }
+function check() {
+    img = document.getElementById("captured_img");
+    classifier.classify(img, gotResult);
+  }
+  
+  function gotResult(error, results) {
+      if(error){
+          console.error(error);
+      }
+      else{
+          console.log(results)
+          document.getElementById("result_object_name").innerHTML = results[0].label;
+          document.getElementById("result_object_accuracy").innerHTML = results[0].confidence.toFixed(2);
+      }
+  }
